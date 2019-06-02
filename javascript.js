5106966fc8b804b1a7e7aa53603989a2d8a650d2
Kare Salo <kare.salo@e21.fi>
2019-06-02 13:32:33 +0300

    Removed bad words, added some sort of documentation.

--- javascript.js
+++ javascript.js
@@ -1,31 +1,29 @@
 function colourChanger(event) {
-	//window.innerWidth;
-	//window.innerHeight;
 	// Using screen X and Y here because this is screen based and
-	// I'll change it if it sucks donkey dicks.
-	//event.screenX, event.screenY
+	// I'll change it if it sucks.
 	console.group("DEBUGGING INFO");
 	console.log("event.screenX : ", event.screenX);
 	console.log("event.screenY : ", event.screenY);
 	console.groupEnd();
 	/**
 	* What we're looking for is 
-	* linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)
+	* linear-gradient(to right, #ff0000, #ffa500, #ffff00, #008000, #0000ff, #4b0082, #ee82ee)
 	**/
 
 	// Let's initialize some variables.
-	var red = 0,
-		green = 0,
-		blue = 0;
-	// And the dimensions we have..
 	var centerPoint = [parseInt(window.innerWidth / 2, 10), parseInt(window.innerHeight / 2, 10)];
 	var cssString = "";
 	// My first plan is to use hexes here because I kinda like them.
-	for (var i = 0; i < 7; i++) {
-		//var hex = ""
-		//console.log("X : ", event.screenY.toString(16).toUpperCase());
-		//console.log("Y : ", event.screenX.toString(16).toUpperCase());
-		var hexValue = (i < 6) ? "#" + event.screenY.toString(16).toUpperCase() + event.screenX.toString(16).toUpperCase() + ", " : "#" + event.screenY.toString(16).toUpperCase() + event.screenX.toString(16).toUpperCase();
+	for (var i = 1; i < 8; i++) {
+		// To generate different colours, we need some math.
+
+		var y = 20 * i;//(event.screenY * i) / 255;
+		var x = 20 * i;//(event.screenX * i) / 255;
+
+		console.log(parseInt(centerPoint[0] * (event.screenY * i) / 255) / 16);
+		console.log(parseInt(centerPoint[1] * (event.screenX * i) / 255) / 16);
+
+		var hexValue = (i < 7) ? "#" + y.toString(16).toUpperCase() + x.toString(16).toUpperCase() + ", " : "#" + y.toString(16).toUpperCase() + x.toString(16).toUpperCase();
 		cssString = cssString + hexValue;
 	}
 	console.log("cssString = ", cssString);
